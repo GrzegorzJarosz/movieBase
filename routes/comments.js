1ad5f4ee024e1ca3c17dbaf812a22fc05f72012e
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-02-16 10:01:27 +0100

    add simple get

--- routes/comments.js
+++ routes/comments.js
@@ -2,10 +2,18 @@ const express = require ('express');
 const router = express.Router();
 const config = require('../config/database');
 
+const Comment  = require('../models/comment');
+const Movie  = require('../models/movie');
 
 /*-----------------------get-----------------------------------*/
 router.get('/',(req,res)=>{
-
+	Comment.getComments((err,dataz)=>{
+		if(err){
+			throw err;
+			console.log(err);
+		}
+		res.json(dataz);
+	});
 });
 
 /*-----------------------post-----------------------------------*/
--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,20 @@
 const express = require ('express');
 const router = express.Router();
 const config = require('../config/database');
+const Movie  = require('../models/movie');
+//
+const remote = require('../ext/remote');
 
 /*-----------------------get-----------------------------------*/
 router.get('/',(req,res)=>{
-
+	Movie.getMovies((err,movies)=>{
+		if(err){
+			throw err;
+			console.log(err);
+		}
+		res.json(movies);
+	});
 });
-
 
 /*-----------------------post-----------------------------------*/
 router.post('/',(req,res)=>{
